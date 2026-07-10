3443ee3ef31273378230c750a7071430ad40a16e
yo-huaca95 <yo.huaca@udla.edu.co>
2026-07-10 17:23:48 -0500

    Se agregan nuevos componentes y rutas

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -3,6 +3,7 @@ import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
 import { ByCountryPage } from './pages/by-country-page/by-country-page';
 import { ByRegionPage } from './pages/by-region-page/by-region-page';
+import { CountryPage } from './pages/country-page/country-page';
 
 export const countryRoutes: Routes = [
     {
@@ -21,6 +22,10 @@ export const countryRoutes: Routes = [
              {
                 path:'by-region',
                 component:ByRegionPage
+            },
+            {
+                path:'by/:code',
+                component:CountryPage
             },
             {
                 path:'**',
---
+++ src/app/country/pages/country-page/country-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-page',
+  imports: [],
+  templateUrl: './country-page.html',
+})
+export class CountryPage {}
