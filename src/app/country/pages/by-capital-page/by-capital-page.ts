562930a49ea840d727e2724f2a01d8f8f3041583
yo-huaca95 <yo.huaca@udla.edu.co>
2026-07-19 22:41:33 -0500

    Se mejora en codigo empleado se pasa de (resource de @angular/core) -> (rxResource de @angular/core/rxjs-interop )

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,9 +1,11 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {rxResource} from '@angular/core/rxjs-interop';
+
 import { SearchInput } from '../../components/search-input/search-input';
 import { CountryList } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country.service';
 
-import { firstValueFrom } from 'rxjs';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -15,15 +17,24 @@ export class ByCapitalPage {
   countryService= inject(CountryService);
   query=signal('');
 
-  countryResources= resource({
+  countryResources= rxResource({
     params: () => ({query: this.query()}),
-    loader: async({params}) => { // {params,abortSignal,previous} destructuración
-      if(!params.query) return [];
-      return await firstValueFrom( //firstValueFrom de rxjs convierte un observable en una promesa
-        this.countryService.searByCapital(params.query) //observable a convertir
-      )
+    stream:({params})=>{
+      if(!params.query) return of([]);
+      return this.countryService.searByCapital(params.query) //observable
     }
   })
+
+
+  // countryResources= resource({ //resource con @angular/core
+  //   params: () => ({query: this.query()}),
+  //   loader: async({params}) => { // {params,abortSignal,previous} destructuración
+  //     if(!params.query) return [];
+  //     return await firstValueFrom( //firstValueFrom de rxjs convierte un observable en una promesa
+  //       this.countryService.searByCapital(params.query) //observable a convertir
+  //     )
+  //   }
+  // })
 
 
 //   isLoading= signal(false);
