4f4ab3c1f2f5acd8d6f424f0e13f3cf484da4750
yo-huaca95 <yo.huaca@udla.edu.co>
2026-07-23 15:41:04 -0500

    Se implementa un nuevo metodo en el servicio para buscar el pais por su codigo de pais, ademas se hace uso de la función delay para mejora d ela experiencia de usuario

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, throwError } from 'rxjs';
 import { CountryMapper } from '../mapper/country.mapper';
 import { Country } from '../interfaces/country.interface';
 
@@ -29,6 +29,7 @@ export class CountryService {
     )
     .pipe(
       map(CountryMapper.mapRestCoutriesObjectsArrayToCountriesArray),
+      delay(3000),
       catchError(error=>{
           //console.log('error Fetchig', error);
           return throwError(()=>new Error(`No se pudo obtener paises con ese query ${query.length>0? query: 'Vacio'}`))
@@ -53,6 +54,7 @@ export class CountryService {
     )
     .pipe(
       map(CountryMapper.mapRestCoutriesObjectsArrayToCountriesArray),
+      delay(3000),
       catchError(error=>{
           //console.log('error Fetchig', error);
           return throwError(()=>new Error(`No se pudo obtener paises con ese query ${query.length>0? query: 'Vacio'}`))
@@ -77,10 +79,38 @@ export class CountryService {
     )
     .pipe(
       map(CountryMapper.mapRestCoutriesObjectsArrayToCountriesArray),
+      delay(3000),
       catchError(error=>{
           //console.log('error Fetchig', error);
           return throwError(()=>new Error(`No se pudo obtener paises con ese query ${query.length>0? query: 'Vacio'}`))
       })
     )
   }
+
+  searByAlphaCode(code:string){
+    code=code.toLowerCase();
+    //console.log(query);
+    return this.http.get<RESTCountry>(`${environment.countryUrl}/codes.alpha_3`
+     ,{
+       params:{
+        q:code,
+        
+       },
+       headers:{
+        // 'Content-Type': 'application/json',
+        'Authorization': `Bearer ${environment.countryApiKey}`
+       }
+    }
+    )
+    .pipe(
+      map(CountryMapper.mapRestCoutriesObjectsArrayToCountriesArray),
+      map(countries => countries.at(0)),
+      delay(3000),
+      catchError(error=>{
+          //console.log('error Fetchig', error);
+          return throwError(()=>new Error(`No se pudo obtener paises con ese query ${code.length>0? code: 'Vacio'}`))
+      })
+    )
+  }
+
 }
